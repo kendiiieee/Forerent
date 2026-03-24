443789005fbbde08f910c06c5a0080f45f20affd
jeh <abrerajohnrey@gmail.com>
2026-03-24 16:13:41 +0800

    forecast fix

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@ chmod -R 775 /var/www/bootstrap/cache
 php artisan migrate:fresh --force
 php artisan config:cache
 php artisan storage:link
+php artisan config:clear
+php artisan view:clear
+php artisan cache:clear
+php artisan livewire:discover
 
 # Seed in background so supervisord starts immediately
 php artisan db:seed --force &
